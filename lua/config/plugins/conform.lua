99cf768b8300e7f8b96c4a686ee7221e60e893ee
HPro <prosserhamish@gmail.com>
2024-12-26 18:14:18 +1100

    feat: update formatters to use conform plugin

---
+++ lua/config/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          go = { "gofmt" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        },
+        default_format_opts = {
+          lsp_format = "fallback",
+        },
+      })
+    end,
+  },
+}
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -4,20 +4,25 @@ return {
     dependencies = {},
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = {},
     config = function()
-      require("mason-lspconfig").setup()
-    end
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+        },
+      })
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      'saghen/blink.cmp',
-      'williamboman/mason-lspconfig',
+      "saghen/blink.cmp",
+      "williamboman/mason-lspconfig",
       {
         "folke/lazydev.nvim",
         opts = {
@@ -26,39 +31,42 @@ return {
           },
         },
       },
+      "stevearc/conform.nvim",
     },
     config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      require("mason-lspconfig").setup_handlers {
+      require("mason-lspconfig").setup_handlers({
         function(server_name)
-          require("lspconfig")[server_name].setup { capabilities = capabilities }
-        end
-      }
+          require("lspconfig")[server_name].setup({ capabilities = capabilities })
+        end,
+      })
 
       local nmap = function(keys, func, desc)
         if desc then
-          desc = 'LSP: ' .. desc
+          desc = "LSP: " .. desc
         end
-        vim.keymap.set('n', keys, func, { desc = desc })
+        vim.keymap.set("n", keys, func, { desc = desc })
       end
 
-      local builtin = require('telescope.builtin')
-      nmap('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-      nmap('gr', builtin.lsp_references, '[G]oto [R]eferences')
-      nmap('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-      nmap('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
-      nmap('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-      nmap('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+      local builtin = require("telescope.builtin")
+      nmap("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+      nmap("gr", builtin.lsp_references, "[G]oto [R]eferences")
+      nmap("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+      nmap("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+      nmap("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+      nmap("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-      vim.api.nvim_create_autocmd('LspAttach', {
+      vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then return end
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = args.buf,
+          if not c then
+            return
+          end
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
             callback = function()
-              vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+              require("conform").format({ bufnr = args.buf, id = c.id })
             end,
           })
         end,
